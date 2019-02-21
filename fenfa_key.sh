3cce0ac9ee4b9c7146d03b95a110b828a9d6fb62
dujincan <jincan.du@outlook.com>
2019-02-21 17:26:15 +0800

    modify fenfa_key.sh

--- fenfa_key.sh
+++ fenfa_key.sh
@@ -4,19 +4,21 @@
 # 2018 10 11
 # v1.0
 
-
+password=111111
+hostip=/etc/ansible/put_sshkey/hostip.txt
+sshport=22
+yum install -y sshpass
 # rm old key
-#rm -f /root/.ssh/id_dsa*
+rm -f /root/.ssh/id_dsa*
 
 # create new key
 ssh-keygen -t dsa -f /root/.ssh/id_dsa -N ""
 
 # put key to host
-file=/server/scripts/fenfa_key/hostip.txt
-sshport=22
 
-for ip in `cat $file`
+
+for ip in `cat $hostip`
 do
-sshpass -p123456 ssh-copy-id -i /root/.ssh/id_dsa.pub "-o StrictHostKeyChecking=no -p$sshport $ip"   # centos 6
-#sshpass -p123456 ssh-copy-id -i /root/.ssh/id_dsa.pub -o StrictHostKeyChecking=no  $ip # centos7
+#sshpass -p$password ssh-copy-id -i /root/.ssh/id_dsa.pub "-o StrictHostKeyChecking=no -p$sshport $ip"   # centos 6
+sshpass -p$password ssh-copy-id -i /root/.ssh/id_dsa.pub -o StrictHostKeyChecking=no  $ip # centos7
 done
